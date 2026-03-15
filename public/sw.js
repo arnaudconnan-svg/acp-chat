87667edc3b377ceb0be03f940ddd3dd5cb8e5970
arnaudconnan-svg <arnaud.connan@gmail.com>
2026-03-15 11:31:13 +0100

    update

--- public/sw.js
+++ public/sw.js
@@ -1,2 +1,41 @@
-self.addEventListener("install", () => self.skipWaiting());
-self.addEventListener("activate", () => self.clients.claim());
+const CACHE_NAME = "facilitatio";
+
+const urlsToCache = [
+  "/",
+  "/index.html",
+  "/app-config.js",
+  "/manifest.json",
+  "/images/logo.png",
+  "/images/icon-192.png",
+  "/images/icon-512.png"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames =>
+      Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      )
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(cachedResponse => {
+      return cachedResponse || fetch(event.request);
+    })
+  );
+});
