19683fc6ac657774da7bb87d0ab5a2ce0b4c0e66
arnaudconnan-svg <arnaud.connan@gmail.com>
2026-03-25 13:11:08 +0100

    update

--- server.js
+++ server.js
@@ -1,5 +1,12 @@
 require("dotenv").config();
 
+const admin = require("firebase-admin");
+const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: process.env.FIREBASE_DATABASE_URL
+});
+const db = admin.database();
 const crypto = require("crypto");
 const ADMIN_PASSWORD = process.env.ADMIN_PASSWORD;
 const SESSION_SECRET = process.env.SESSION_SECRET;
